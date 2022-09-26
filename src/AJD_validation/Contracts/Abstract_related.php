a5552426db780971d142850f873fc69575a9cb40
ajdoc <108965281+ajdoc@users.noreply.github.com>
2022-09-26 11:37:14 +0800

    Update Abstract_related.php

--- src/AJD_validation/Contracts/Abstract_related.php
+++ src/AJD_validation/Contracts/Abstract_related.php
@@ -7,16 +7,16 @@ use AJD_validation\Contracts\Abstract_exceptions;
 
 abstract class Abstract_related extends Abstract_rule
 {
-	public $mandatory = true;
-	public $relation = '';
-	public $validator;
+    public $mandatory = true;
+    public $relation = '';
+    public $validator;
 
- 	abstract public function hasRelation($value);
+    abstract public function hasRelation($value);
 
     abstract public function getRelationValue($value);
 
-	public function __construct($relation, Rule_interface $validator = null, $mandatory = true)
-	{
+    public function __construct($relation, Rule_interface $validator = null, $mandatory = true)
+    {
         $this->setName($relation);
 
         if( $validator AND !$validator->getName() ) 
@@ -27,43 +27,50 @@ abstract class Abstract_related extends Abstract_rule
         $this->relation = $relation;
         $this->validator = $validator;
         $this->mandatory = $mandatory;
-	}
+    }
     
-	public function setName($name)
-	{
-	    parent::setName($name);
+    public function setName($name)
+    {
+        parent::setName($name);
 
-	    if($this->validator instanceof Rule_interface) 
-	    {
-	        $this->validator->setName($name);
-	    }
+        if($this->validator instanceof Rule_interface) 
+        {
+            $this->validator->setName($name);
+        }
 
-	    return $this;
-	}
+        return $this;
+    }
 
-	private function decision($type, $hasRelation, $value, $override = false)
+    private function decision($type, $hasRelation, $value, $override = false)
     {
         $relationValue = $this->getRelationValue( $value );
 
-    	return ( !$this->mandatory && !$hasRelation )
-				    || ( IS_NULL( $this->validator ) 
-					   || ( $type === 'assertErr' ) ? $this->validator->{$type}( $relationValue, $override ) : $this->validator->{$type}( $relationValue )
-				);
+        $typeResult = true;
+
+        if(!is_null($this->validator))
+        {
+            $typeResult = ( $type === 'assertErr' ) ? $this->validator->{$type}( $relationValue, $override ) : $this->validator->{$type}( $relationValue );
+        }
+
+        return ( !$this->mandatory && !$hasRelation )
+                    || ( is_null( $this->validator ) 
+                       || $typeResult
+                );
     }
 
     public function run( $value, $satisfier = null, $field = null, $clean_field = null )
     {
-    	$hasRelation = $this->hasRelation($value);
-    	$check = false;
+        $hasRelation = $this->hasRelation($value);
+        $check = false;
         $append_error = '';
 
-    	if( $this->mandatory && !$hasRelation ) 
-    	{
-    		$check = false;
-    	}
-    	else
-    	{
-    		$check = $this->decision( 'run', $hasRelation, $value );
+        if( $this->mandatory && !$hasRelation ) 
+        {
+            $check = false;
+        }
+        else
+        {
+            $check = $this->decision( 'run', $hasRelation, $value );
 
             if( !$check )
             {
@@ -104,7 +111,7 @@ abstract class Abstract_related extends Abstract_rule
                     });
                 }
             }
-    	}
+        }
 
         if(!empty($append_error))
         {
@@ -123,7 +130,7 @@ abstract class Abstract_related extends Abstract_rule
 
     public function validate($value)
     {
-	 	$check = $this->run( $value );
+        $check = $this->run( $value );
 
         if( is_array( $check ) )
         {
